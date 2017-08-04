52d76ebb49f8b89760a54ee2df72723741093046
dawoodkhan82 <dawoodkhan82@gmail.com>
2017-08-04 00:44:29 -0400

    Initial Commit

---
+++ TipMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipMe
+//
+//  Created by Dawood Khan on 8/4/17.
+//  Copyright © 2017 Dawood Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
